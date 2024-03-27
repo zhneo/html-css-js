52fac5a8539e5de4ed431a8328bd6bc9d766b575
Uali <zharkinbai99@gmail.com>
2024-03-28 03:35:08 +0500

    Added playlist img and search for 2004

---
+++ playlist/playlist_2004.js
@@ -0,0 +1,7 @@
+function goToBack(icon) {
+    if (icon === 'left') {
+        window.location.href = '/main_page/main_menu.html';
+    }else if (icon === 'bars') {
+        window.location.href = '#';
+    }
+}
